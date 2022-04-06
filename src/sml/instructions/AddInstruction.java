4d4b810cbea371a27dba90d6acfaf88dfa734d85
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-04-06 15:33:54 +0100

    added AddInstruction execute method

--- src/sml/instructions/AddInstruction.java
+++ src/sml/instructions/AddInstruction.java
@@ -9,13 +9,26 @@ import sml.Machine;
  * @author ...
  */
 public class AddInstruction extends Instruction {
+    int register;
+    int s1;
+    int s2;
 
     public AddInstruction(String label, int register, int s1, int s2){
-        super("Something", "Something Else");
+        super(label, "add");
+        this.register = register;
+        this.s1 = s1;
+        this.s2 = s2;
     }
 
     @Override
     public void execute(Machine m) {
-        // TODO: Implement
+
+        int n1 = m.getRegisters().getRegister(this.s1);
+        int n2 = m.getRegisters().getRegister(this.s2);
+
+        int sum = n1+n2;
+
+        m.getRegisters().setRegister(this.register, sum);
+
     }
 }
