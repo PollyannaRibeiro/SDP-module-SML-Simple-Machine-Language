9a978434a41750c3146ba8ba1598efec8fe85ba7
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-04-07 11:44:04 +0100

    added bnz execute method

--- src/sml/instructions/BnzInstruction.java
+++ src/sml/instructions/BnzInstruction.java
@@ -1,17 +1,28 @@
 package sml.instructions;
 
+import sml.AccessLabel;
 import sml.Instruction;
 import sml.Machine;
 
 public class BnzInstruction extends Instruction {
-    public BnzInstruction(String label, int register, int s1, int s2) {
-        super(label, "out");
+    private int s1;
+    private String l2;
+
+    public BnzInstruction(String label, int s1, String L2) {
+        super(label, "bnz");
+        this.s1 = s1;
+        this.l2 = L2;
     }
 
     @Override
     public void execute(Machine m) {
 
-        // TODO: Implement
+        int n = m.getRegisters().getRegister(this.s1);
+        if (n != 0){
 
+            AccessLabel access = new AccessLabel(l2);
+            int indexLabel = access.getLabelIndexOf(m);
+            m.setPc(indexLabel);
+        }
     }
 }
